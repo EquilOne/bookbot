210d2cbd0a1ffd3008440b1bb69b271978fc906e
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-11-10 06:06:02 -0700

    Change main.py to count words in book

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
+
+
+def count_words(string):
+    num_words = len(string.split())
+    return num_words
+
+
+print(f"Found {count_words(get_book_text('./books/frankenstein.txt'))} total words")
