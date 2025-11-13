04b1235bbcea58a4bb56dab45eb92885a2609653
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-11-13 13:04:33 -0700

    Feature bookbot command line argument

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
+import sys
 from stats import count_words
 from stats import get_num_chars
 from stats import get_char_list
 from stats import sort_on
 
-frankenstein = "./books/frankenstein.txt"
+
+def run_book_bot(path_to_book):
+    book_text = get_book_text(path_to_book)
+    word_count = count_words(book_text)
+    sorted_list = get_sorted_list(book_text)
+    print_report(path_to_book, word_count, sorted_list)
 
 
-def get_book_text(path_to_file):
-    with open(path_to_file) as f:
+def get_book_text(path_to_book):
+    with open(path_to_book) as f:
         file_contents = f.read()
     return file_contents
 
 
-def print_report():
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {frankenstein}...")
-    print("----------- Word Count ----------")
-    print(f"Found {count_words(get_book_text(frankenstein))} total words")
-    print("-------- Character Count --------")
-    for item in char_list:
+def get_sorted_list(book_text: str):
+    char_count = get_num_chars(book_text)
+    char_list = get_char_list(char_count)
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
+
+def print_report(path_to_book, word_count, list):
+    print("================= BOOKBOT =================\n")
+    print(f"Analyzing book found at: {path_to_book}...\n")
+    print("---------------- Word Count ---------------\n")
+    print(f"Found {word_count} total words\n")
+    print("------------- Character Count -------------\n")
+    for item in list:
         print(f"{item['char']}: {item['num']}")
 
 
-char_count_dict = get_num_chars(get_book_text(frankenstein))
-
-char_list = get_char_list(char_count_dict)
-
-char_list.sort(reverse=True, key=sort_on)
-
-print_report()
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    run_book_bot(sys.argv[1])
