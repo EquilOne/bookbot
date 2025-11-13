d64c39777d60dd1f9cba698e5c74f1e36388dc54
Chase Kelly <188378569+EquilOne@users.noreply.github.com>
2025-11-13 09:40:25 -0700

    Feature print report on text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import count_words
 from stats import get_num_chars
-# from stats import sort_on
+from stats import get_char_list
+from stats import sort_on
 
 frankenstein = "./books/frankenstein.txt"
 
@@ -11,11 +12,21 @@ def get_book_text(path_to_file):
     return file_contents
 
 
-char_count_list = get_num_chars(get_book_text(frankenstein))
+def print_report():
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {frankenstein}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(get_book_text(frankenstein))} total words")
+    print("-------- Character Count --------")
+    for item in char_list:
+        print(f"{item['char']}: {item['num']}")
 
-print("============ BOOKBOT ============")
-print(f"Analyzing book found at {frankenstein}...")
-print("----------- Word Count ----------")
-print(f"Found {count_words(get_book_text(frankenstein))} total words")
-print("-------- Character Count --------")
-print(f"List of characters in text: {char_count_list}")
+
+char_count_dict = get_num_chars(get_book_text(frankenstein))
+
+char_list = get_char_list(char_count_dict)
+
+char_list.sort(reverse=True, key=sort_on)
+
+print_report()
+# print(f"{char_list[0]['char']}: {char_list[0]['num']}")
--- stats.py
+++ stats.py
@@ -6,13 +6,23 @@ def count_words(string):
 def get_num_chars(string):
     char_count = {}
     for char in string:
-        char = char.lower()
-        if char not in char_count:
-            char_count[char] = 1
+        char_low = char.lower()
+        if not char_low.isalpha():
+            continue
+        elif char_low not in char_count:
+            char_count[char_low] = 1
         else:
-            char_count[char] += 1
+            char_count[char_low] += 1
     return char_count
 
 
-def sort_on(items):
-    return items["char"]
+def sort_on(entry):
+    return entry["num"]
+
+
+def get_char_list(dict):
+    char_list = []
+    for k, v in dict.items():
+        char_dict = {"char": k, "num": v}
+        char_list.append(char_dict)
+    return char_list
